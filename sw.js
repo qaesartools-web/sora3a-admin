17e9a1b0e7d56dafd9b6f80dce110cad4d45f7db
qaesartools-web <qaesartools@gmail.com>
2026-06-01 01:56:12 +0300

    Nuclear SW reset

--- sw.js
+++ sw.js
@@ -1,27 +1,8 @@
-// Service Worker - sora3a-admin only
-const CACHE = 'sora3a-admin-v2';
-const SCOPE = '/sora3a-admin/';
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll([SCOPE])));
-});
-
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', e => {
-  // Only handle requests within sora3a-admin scope
-  if (!e.request.url.includes('/sora3a-admin/')) {
-    e.respondWith(fetch(e.request));
-    return;
-  }
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    caches.keys().then(k => Promise.all(k.map(n => caches.delete(n))))
+    .then(() => self.registration.unregister())
+    .then(() => self.clients.matchAll()).then(clients => clients.forEach(c => c.navigate(c.url)))
   );
 });
